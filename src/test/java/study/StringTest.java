84282a9fde2f182c2aadd3fc25da6c42216dc53f
tjwjdgks <tjwjdgks43@gmail.com>
2022-05-12 23:13:56 +0900

    create test string

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,8 +1,9 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 public class StringTest {
     @Test
@@ -10,4 +11,42 @@ public class StringTest {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
     }
+    @Test
+    void splitDone(){
+        String[] actual = "1,2".split(",");
+        assertThat(actual).contains("1","2");
+    }
+    @Test
+    void splitNotDone(){
+        String[] actual = "1".split(",");
+        assertThat(actual).containsExactly("1");
+    }
+    @Test
+    void subString(){
+        String actual = "(1,2)".substring(1,4);
+        assertThat(actual).isEqualTo("1,2");
+    }
+    @DisplayName("위치값 가져오기")
+    @Test
+    void charAt(){
+        char c = "abc".charAt(0);
+        assertThat(c).isEqualTo('a');
+    }
+    @DisplayName("위치값 exception 1")
+    @Test
+    void charAtErrorV1(){
+        assertThatThrownBy(()->{
+            char c = "abc".charAt(4);
+        }).isInstanceOf(IndexOutOfBoundsException.class)
+          .hasMessage("String index out of range: 4");
+    }
+    @DisplayName("위치값 exception 2")
+    @Test
+    void charAtErrorV2(){
+        assertThatExceptionOfType(IndexOutOfBoundsException.class)
+                .isThrownBy(()->{
+                    char c = "abc".charAt(4);
+                }).withMessageMatching("String index out of range: \\d+");
+    }
 }
+
